a45f00e72e95b0dd8ed51b6506c9a35ed8f15f83
Bruskyy <bruschivitor@gmail.com>
2023-03-24 21:19:58 -0300

    formulario pronto

--- teste/src/app/pages/login/Login.tsx
+++ teste/src/app/pages/login/Login.tsx
@@ -1,18 +1,41 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-export const Login = () =>{
-    
-const navigate = useNavigate()
+export const Login = () => {
 
-const handleClick = () => {
-    navigate('./pagina-inicial')
-}
-    
-    return(
+    const navigate = useNavigate()
+
+    const handleClick = () => {
+        navigate('./pagina-inicial')
+    }
+
+    const handleEntrar = () =>{
+        console.log('E-mail: ' + email)
+        console.log('Senha: ' + senha)
+    }
+    const [email, setEmail] = useState('')
+    const [senha, setPassword] = useState('')
+
+    return (
         <div>
-            Login
+            <form>
+                <label>
+                    <span>E-mail</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+
+                <label>
+                    <span>
+                        Senha
+                    </span>
+                    <input type="password" value={senha} onChange={e => setPassword(e.target.value)} />
+                </label>
+            </form>
             
-            <button onClick={handleClick}>Página Inicial</button>
+            <button type="button" onClick={handleEntrar}>Entrar</button>
+            <div>
+                <button onClick={handleClick}>Página Inicial</button>
+            </div>
         </div>
 
     )
